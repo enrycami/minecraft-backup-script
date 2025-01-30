427cbe1a02ff062de44aa93a868e1007e67d222c
Enrico Camillieri <enrico.camillieri@gmail.com>
2025-01-30 09:19:49 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # minecraft-backup-script
 A simple script to backup my minecraft world
+
+This script will create a backup of the indicated folder by archiving it in a tarball, appending the current date and time to the filename. It will place said tarball in a folder in the same path as the location of the script file.
+After that, it will check for the number of backups in the folder and delete the oldest one if there are more than the max number set on line 25.
+If you'd like to use this, remember to modify line 13, indicating the correct path for your Minecraft world.
---
+++ minecraft-backup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# get current date and create the string to append
+now="$(date)"
+echo "Current date and time: $now"
+now="$(date +'%Y-%m-%d--%H-%M')"
+echo "The following string will be appended to the backup: $now"
+
+# create the backup folder if it does not exist
+mkdir -p minecraft-backups
+
+# create a new backup
+sudo tar -cvf minecraft-backups/bedrock-level-backup-$now.tar -C /opt/bedrock-server/worlds/ 'Bedrock level'
+echo "Backup completed. Checking for older backups to delete..."
+
+# define backup directory
+backup_dir="minecraft-backups"
+
+# check the number of backups
+echo "Checking files in $backup_dir..."
+backup_count=$(find "$backup_dir" -type f -name "*.tar" | wc -l)
+echo "There are $backup_count backups."
+
+# set max number of backups to keep
+max_backups=14
+
+if [ "$backup_count" -gt "$max_backups" ]; then
+    echo "Too many backups. Deleting the oldest one..."
+    # find and delete the oldest file
+    oldest_backup=$(find "$backup_dir" -type f -name "*.tar" -printf "%T+ %p\n" | sort | head -n 1 | awk '{print $2}')
+    if [ -n "$oldest_backup" ]; then
+        echo "Deleting $oldest_backup..."
+        rm -f "$oldest_backup"
+        echo "Oldest backup deleted."
+    else
+        echo "No backup found to delete"
+    fi
+else
+    echo "Backup count is within the limit of $max_backups"
+fi
+
+echo "Backup script completed."
